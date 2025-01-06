f6041738dd6d14b5c29cf683510c6be51a89fec2
Sonali Landge <sonalilandge13@gmail.com>
2025-01-06 08:01:08 +0530

    added method

--- Batch14oops/sonali.cs
+++ Batch14oops/sonali.cs
@@ -5,7 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Batch14oops
-{
+{   public class Test5
+    {
+        public void Display()
+        { 
+        }
+
+    }
     internal class sonali
     {
     }
