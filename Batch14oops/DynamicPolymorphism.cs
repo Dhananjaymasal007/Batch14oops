1ec26dfc3cbf537edc2d103a389e262c84e51f17
Dhananjay Masal <dhananjay9096@gmail.com>
2025-01-08 08:42:43 +0530

    Polymorphism with Examples

---
+++ Batch14oops/DynamicPolymorphism.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch14oops
+{
+
+    //dynamic polymorphism  // method overriding  // late binding
+    //method name same parameter also same but different classes and follows parent child relation ship
+
+    // Base class
+
+    class Shape
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("Drawing shape");
+        }
+    }
+
+
+    //deried class 1
+    class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing circle");
+
+        }
+    }
+    //derived class 2
+    class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing Rectangle");
+        }
+
+    }
+
+    internal class DynamicPolymorphism
+    {
+        static void Main()
+        {
+
+            Shape shape1 = new Shape();
+            Shape shape2 = new Circle();
+            Shape shape3 = new Rectangle();
+
+
+            //calling method
+            shape1.Draw();
+            shape2.Draw();
+            shape3.Draw();
+
+
+        }
+
+
+
+    }
+}
---
+++ Batch14oops/StaticPloymorphism.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch14oops
+{
+    //method name same class also same but diffenent parameter
+    //static polymorphsim or  method overloading or compile  time polymorphism or early binding
+
+    
+    class MathOperations
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public double Add(double a, int b) 
+        {
+         return a + b;
+        }
+    }
+
+    internal class StaticPloymorphism
+    {
+        static void Main()
+        {
+            MathOperations math = new MathOperations();
+
+            // calling the overload methods
+
+            int sum = math.Add(2, 4);
+            double sum1 = math.Add(3.5,4);
+
+            Console.WriteLine("$Sum of Integers : "+sum);
+
+            Console.WriteLine("$Sum of Doubles : "+sum1);
+
+
+        }
+    }
+}
